a8c49017b6ab10953f9ce9c6061bea884d70dfb2
Alyx11 <aleks78.gooogle@gmail.com>
2020-06-08 15:38:37 +0300

    added adaptive

--- src/js/main.js
+++ src/js/main.js
@@ -64,6 +64,10 @@ $(function(){
         return false;
       });
 
+      $('.menu__btn').on('click', function(){
+        $('.menu__list').slideToggle();
+      });
+
 });
 
 function readMore() {
