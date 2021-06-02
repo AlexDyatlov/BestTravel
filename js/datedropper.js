c61b8ec4d3cb71399da4f747e1d5b88152ec3b60
AlexDyatlov <alexdyatlov.web@gmail.com>
2021-06-02 22:40:05 +0300

    Updates

---
+++ js/datedropper.js
@@ -0,0 +1,14 @@
+$('#departure-date').dateDropper({
+  lang: 'ru',
+  format: 'd-m-Y',
+  theme: 'my-theme',
+  large: 'true',
+  largeDefault: 'true',
+  maxYear: '2022',
+  minYear: '2020',
+  lock: 'from',
+  roundtrip: 'my-trip',
+  largeOnly: 'true'
+});
+
+
